7dc0f70de30e99a16967e97af4f021a124c89ed4
Ben Wright <benjamin.p.wright@hotmail.com>
2023-09-19 12:37:06 +0100

    WIP: delete and edit in recipies list

--- src/views/RecipiesView.vue
+++ src/views/RecipiesView.vue
@@ -3,12 +3,23 @@
   import { useRecipieStore } from "../stores/recipies";
   import ObjectList from "../components/ObjectList.vue";
   import NewThing from "@/components/NewThing.vue";
+import type Recipie from "@/models/Recipie";
 
   const store = useRecipieStore();
 
   onMounted(() => {
     store.load();
   });
+
+  async function remove(recipie: Recipie) {
+    await store.removeRecipie(recipie);
+  }
+
+  function edit(recipie: Recipie) {
+    console.info(`Edit ${recipie.name}`);
+    // store.select(recipie);
+    // router.push("/recipies/new/name");
+  }
 </script>
 
 <template>
@@ -16,7 +27,7 @@
     <template v-if="store.loading">Loading...</template>
     <template v-else-if="store.error">{{ store.error }}</template>
     <template v-else>
-      <ObjectList :data="store.recipies" v-slot="slotProps">
+      <ObjectList :data="store.recipies" @delete="remove" @edit="edit" v-slot="slotProps">
         <h2>{{ slotProps.obj.name }}</h2>
       </ObjectList>
       <NewThing to="/recipies/new/name" />
