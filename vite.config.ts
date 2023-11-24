acf40f9d02a9420148c227fe89604f87210c0051
Ben Wright <benjamin.p.wright@hotmail.com>
2023-11-24 09:53:06 +0000

    adding deps for sqlite-wasm and headers to allow it to use OPFS

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp"
+    }
+  },
+  optimizeDeps: {
+    exclude: ['@sqlite.org/sqlite-wasm'],
+  },
   plugins: [
     vue({
       script: {
