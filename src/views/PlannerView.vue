c545dd92dd6724efb74d5b0c82a7139a50f1b5f7
bwright <bwright@roku.com>
2023-12-15 10:49:37 +0000

    refactor for less duplication

--- src/views/PlannerView.vue
+++ src/views/PlannerView.vue
@@ -5,7 +5,7 @@
   import ObjectList from "@/components/ObjectList.vue";
   import NewThing from "@/components/NewThing.vue";
   import PopUp from "@/components/PopUp.vue";
-  import ExpanderButton from "@/components/ExpanderButton.vue";
+  import CollapsibleSection from "@/components/CollapsibleSection.vue";
 
   import { type DinnerPlan, Day, getDinnerPlans, deleteDinnerPlan, clearDinnerPlans } from "@/database/models/dinner-plans";
   import { type NonDinnerPlan, getBreakfastPlans, getLunchPlans, deleteBreakfastPlan, deleteLunchPlan, clearNonDinnerPlans } from "@/database/models/non-dinner-plans";
@@ -15,12 +15,6 @@
 
   const popup = ref(false);
   const loading = ref(true);
-  const expanded = ref({
-    breakfast: true,
-    lunch: true,
-    dinner: true,
-    extras: false,
-  } as Record<string, boolean>);
 
   const breakfasts = ref([] as NonDinnerPlan[]);
   const lunches = ref([] as NonDinnerPlan[]);
@@ -72,14 +66,8 @@
     <template v-if="loading">Loading...</template>
     <template v-else>
       <div class="big-window">
-        <h1>Meal Plans</h1>
-        <section>
-          <span class="horizontal">
-            <h1>Breakfast</h1>
-            <ExpanderButton v-model="expanded['breakfast']" />
-          </span>
-          <template v-if="expanded['breakfast']">
-            <ObjectList
+        <CollapsibleSection title="Breakfast" expanded>
+          <ObjectList
               :data="breakfasts"
               :get-id="d => d.id"
               @delete="p => deleteBreakfast(p.id)"
@@ -96,77 +84,58 @@
               </template>
             </ObjectList>
             <NewThing to="/planner/breakfasts/new" />
-          </template>
-        </section>
-        <section>
-          <span class="horizontal">
-            <h1>Lunch</h1>
-            <ExpanderButton v-model="expanded['lunch']" />
-          </span>
-          <template v-if="expanded['lunch']" >
-            <ObjectList
-              :data="lunches"
-              :get-id="d => d.id"
-              @delete="p => deleteLunch(p.id)"
-              @edit="p => router.push(`/planner/lunches/${ p.id }`)"
-              dropdown
-            >
-              <template #content="{ obj }">
-                <h2>{{ obj.recipieName }}</h2>
-              </template>
-              <template #select-dropdown="{ obj }">
-                <article>
-                  <p>Servings: {{ obj.servings }}</p>
-                </article>
-              </template>
-            </ObjectList>
-            <NewThing to="/planner/lunches/new" />
-          </template>
-        </section>
-        <section>
-          <span class="horizontal">
-            <h1>Dinner</h1>
-            <ExpanderButton v-model="expanded['dinner']" />
-          </span>
-          <template v-if="expanded['dinner']">
-            <ObjectList
-              :data="dinners"
-              :get-id="d => d.id"
-              @delete="p => deletePlan(p.id)"
-              @edit="p => router.push(`/planner/dinners/${ p.id }`)"
-              dropdown
-            >
-              <template #content="{ obj }">
-                <h2>{{ Day[obj.day] }}: {{ obj.recipieName }}</h2>
-              </template>
-              <template #select-dropdown="{ obj }">
-                <article>
-                  <p>Servings: {{ obj.servings }}</p>
-                </article>
-              </template>
-            </ObjectList>
-            <NewThing to="/planner/dinners/new" />
-          </template>
-        </section>
-        <section>
-          <span class="horizontal">
-            <h1>Extras</h1>
-            <ExpanderButton v-model="expanded['extras']" />
-          </span>
-          <template v-if="expanded['extras']">
-            <ObjectList
-              :data="extras"
-              :get-id="e => e.id"
-              @delete="e => deleteExtra(e.id)"
-              @edit="e => router.push(`/planner/extras/${ e.id }`)"
-            >
-              <template #content="{ obj }">
-                <h2>{{ obj.name }}: {{ obj.quantity }}{{ obj.quantity === 1 ? obj.unitSingular : obj.unitPlural }}</h2>
-              </template>
-            </ObjectList>
-            <NewThing to="/planner/extras/new" />
-          </template>
-        </section>
+        </CollapsibleSection>
+        <CollapsibleSection title="Lunch" expanded>
+          <ObjectList
+            :data="lunches"
+            :get-id="d => d.id"
+            @delete="p => deleteLunch(p.id)"
+            @edit="p => router.push(`/planner/lunches/${ p.id }`)"
+            dropdown
+          >
+            <template #content="{ obj }">
+              <h2>{{ obj.recipieName }}</h2>
+            </template>
+            <template #select-dropdown="{ obj }">
+              <article>
+                <p>Servings: {{ obj.servings }}</p>
+              </article>
+            </template>
+          </ObjectList>
+          <NewThing to="/planner/lunches/new" />
+        </CollapsibleSection>
+        <CollapsibleSection title="Dinner" expanded>
+          <ObjectList
+            :data="dinners"
+            :get-id="d => d.id"
+            @delete="p => deletePlan(p.id)"
+            @edit="p => router.push(`/planner/dinners/${ p.id }`)"
+            dropdown
+          >
+            <template #content="{ obj }">
+              <h2>{{ Day[obj.day] }}: {{ obj.recipieName }}</h2>
+            </template>
+            <template #select-dropdown="{ obj }">
+              <article>
+                <p>Servings: {{ obj.servings }}</p>
+              </article>
+            </template>
+          </ObjectList>
+          <NewThing to="/planner/dinners/new" />
+        </CollapsibleSection>
+        <CollapsibleSection title="Extras">
+          <ObjectList
+            :data="extras"
+            :get-id="e => e.id"
+            @delete="e => deleteExtra(e.id)"
+            @edit="e => router.push(`/planner/extras/${ e.id }`)"
+          >
+            <template #content="{ obj }">
+              <h2>{{ obj.name }}: {{ obj.quantity }}{{ obj.quantity === 1 ? obj.unitSingular : obj.unitPlural }}</h2>
+            </template>
+          </ObjectList>
+          <NewThing to="/planner/extras/new" />
+        </CollapsibleSection>
       </div>
       <button type="reset" @click.stop="popup = !popup">Reset</button>
       <PopUp v-if="popup" message="Delete you meal plans?" @submit="clear()" @cancel="popup = !popup" />
@@ -175,15 +144,6 @@
 </template>
 
 <style scoped>
-  h1 {
-    flex-grow: 1;
-  }
-
-  h2 {
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-
   article {
     padding: 0.5em;
   }
