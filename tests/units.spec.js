70e924a3ac0ffaacacaa280c468d7a934c74a0c2
Ben Wright <benjamin.p.wright@hotmail.com>
2024-03-18 18:48:32 +0000

    tweaking permissions

--- tests/units.spec.js
+++ tests/units.spec.js
@@ -4,8 +4,8 @@ import { test, expect } from '@playwright/test';
 test('can see units', async ({ page }) => {
   await page.goto('/units');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Nom Nom/);
-
   await expect(page.getByRole('heading', { name: 'Units' })).toBeVisible();
+
+  const rows = await page.$$('table tbody tr');
+  expect(rows).toHaveLength(2);
 });
